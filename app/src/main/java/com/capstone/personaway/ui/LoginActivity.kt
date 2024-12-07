471a280f735cd07a8721646f3e476e9a1fc25208
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2024-12-07 19:22:44 +0700

    add: firebase auth login

--- app/src/main/java/com/capstone/personaway/ui/LoginActivity.kt
+++ app/src/main/java/com/capstone/personaway/ui/LoginActivity.kt
@@ -2,15 +2,20 @@ package com.capstone.personaway.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.personaway.R
 import com.capstone.personaway.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +27,25 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun setupAction() {
+        auth = Firebase.auth
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.edLoginEmail.text.toString()
+            val password = binding.edLoginPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(baseContext, "Welcome!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        binding.edLoginEmail.text!!.clear()
+                        binding.edLoginPassword.text!!.clear()
+                    }
+                }
+        }
+
         binding.registerTextView.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
