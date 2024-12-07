d1f0c6e093685b518d045f0e992d490b848bc8ca
atzulf <atzulfikar45@gmail.com>
2024-12-08 00:34:19 +0700

    Slicing UI and Add Bottom bar

--- app/src/main/java/com/capstone/personaway/ui/MainActivity.kt
+++ app/src/main/java/com/capstone/personaway/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.capstone.personaway.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,11 +30,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        bottombar()
         setupAction()
     }
 
     private fun setupAction() {
         auth = Firebase.auth
         binding.welcomeUser.text = "Welcome, ${auth.currentUser!!.displayName}"
+    }
+
+    private fun bottombar() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    // Pindah ke HomeActivity
+                    startActivity(Intent(this, MainActivity::class.java))
+                    true
+                }
+                R.id.nav_test -> {
+                    startActivity(Intent(this, TestActivity::class.java))
+                    true
+                }
+                R.id.nav_result -> {
+                    // Pindah ke NotificationsActivity
+                    startActivity(Intent(this, ResultActivity::class.java))
+                    true
+                }
+                R.id.nav_profile -> {
+                    // Pindah ke ProfileActivity
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
--- app/src/main/java/com/capstone/personaway/ui/TestActivity.kt
+++ app/src/main/java/com/capstone/personaway/ui/TestActivity.kt
@@ -1,7 +1,9 @@
 package com.capstone.personaway.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.capstone.personaway.R
 import com.capstone.personaway.databinding.ActivityTestBinding
 
 class TestActivity : AppCompatActivity() {
@@ -12,5 +14,45 @@ class TestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        bottombar()
+    }
+
+    private fun bottombar() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    // Pindah ke HomeActivity
+                    if (javaClass != MainActivity::class.java) { // Periksa kelas saat ini
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish() // Tutup aktivitas saat ini
+                    }
+                    true
+                }
+                R.id.nav_test -> {
+                    if (javaClass != TestActivity::class.java) { // Periksa kelas saat ini
+                        startActivity(Intent(this, TestActivity::class.java))
+                        finish() // Tutup aktivitas saat ini
+                    }
+                    true
+                }
+                R.id.nav_result -> {
+                    // Pindah ke ResultActivity
+                    if (javaClass != ResultActivity::class.java) { // Periksa kelas saat ini
+                        startActivity(Intent(this, ResultActivity::class.java))
+                        finish()
+                    }
+                    true
+                }
+                R.id.nav_profile -> {
+                    // Pindah ke ProfileActivity
+                    if (javaClass != ProfileActivity::class.java) { // Periksa kelas saat ini
+                        startActivity(Intent(this, ProfileActivity::class.java))
+                        finish()
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
