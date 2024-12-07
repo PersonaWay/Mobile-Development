829f350213449cd707d876f769be9d4f5d309025
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2024-12-07 19:37:37 +0700

    fix: user display name

--- app/src/main/java/com/capstone/personaway/ui/MainActivity.kt
+++ app/src/main/java/com/capstone/personaway/ui/MainActivity.kt
@@ -6,8 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.personaway.R
+import com.capstone.personaway.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +24,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAction()
+    }
+
+    private fun setupAction() {
+        auth = Firebase.auth
+        binding.welcomeUser.text = "Welcome, ${auth.currentUser!!.displayName}"
     }
 }
