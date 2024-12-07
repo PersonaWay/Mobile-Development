e3ee31ff18f87d07f1267f504ddbce8550b5462b
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2024-12-07 19:12:14 +0700

    add: firebase auth

--- app/src/main/java/com/capstone/personaway/ui/RegisterActivity.kt
+++ app/src/main/java/com/capstone/personaway/ui/RegisterActivity.kt
@@ -2,16 +2,23 @@ package com.capstone.personaway.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.personaway.R
 import com.capstone.personaway.databinding.ActivityRegisterBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.userProfileChangeRequest
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +29,33 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun setupAction() {
+        auth = Firebase.auth
+
+        binding.registerButton.setOnClickListener {
+            val username = binding.nameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(baseContext, "Authentication success.", Toast.LENGTH_SHORT).show()
+                        val setUpsername = userProfileChangeRequest {
+                            displayName = username
+                        }
+                        auth.currentUser!!.updateProfile(setUpsername).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                Log.d("RegisterActivity", "User profile updated.")
+                            }
+                        }
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
         binding.loginTextView.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
