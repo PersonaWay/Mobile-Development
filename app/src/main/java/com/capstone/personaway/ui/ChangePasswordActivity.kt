bd8aa637ca7a5803731976690995599eb68f9098
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2024-12-08 21:00:02 +0700

    add: change password activity

---
+++ app/src/main/java/com/capstone/personaway/ui/ChangePasswordActivity.kt
@@ -0,0 +1,16 @@
+package com.capstone.personaway.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.personaway.databinding.ActivityChangePasswordBinding
+
+class ChangePasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChangePasswordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChangePasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
