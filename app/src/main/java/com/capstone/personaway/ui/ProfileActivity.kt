5b9a8740178dbed6345d8747f1311030bb4f34ec
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2024-12-08 22:20:22 +0700

    add: profile activity logic

--- app/src/main/java/com/capstone/personaway/ui/ProfileActivity.kt
+++ app/src/main/java/com/capstone/personaway/ui/ProfileActivity.kt
@@ -1,16 +1,44 @@
 package com.capstone.personaway.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.personaway.databinding.ActivityProfileBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProfileBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupAction()
+    }
+
+    private fun setupAction() {
+        auth = Firebase.auth
+
+        binding.tvName.text = auth.currentUser!!.displayName
+
+        binding.ibBack.setOnClickListener {
+            finish()
+        }
+        binding.btnEdit.setOnClickListener {
+            val intent = Intent(this, EditProfileActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnLogout.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            finish()
+        }
     }
 }
