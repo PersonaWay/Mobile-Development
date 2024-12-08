2c8a7bb4cfa29e98c381a0fdeef6f5bd685dd7ed
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2024-12-08 18:44:54 +0700

    add: edit profile activity

---
+++ app/src/main/java/com/capstone/personaway/ui/EditProfileActivity.kt
@@ -0,0 +1,16 @@
+package com.capstone.personaway.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.personaway.databinding.ActivityEditProfileBinding
+
+class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
