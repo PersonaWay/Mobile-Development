26541d26ec949589ff24519d012d5aecf7e8f6e7
atzulf <atzulfikar45@gmail.com>
2024-12-09 10:48:09 +0700

    Styling test fragment

--- app/src/main/java/com/capstone/personaway/ui/TestActivity.kt
+++ app/src/main/java/com/capstone/personaway/ui/TestActivity.kt
@@ -14,45 +14,7 @@ class TestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        bottombar()
+
     }
 
-    private fun bottombar() {
-        binding.bottomNavigation.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_home -> {
-                    // Pindah ke HomeActivity
-                    if (javaClass != MainActivity::class.java) { // Periksa kelas saat ini
-                        startActivity(Intent(this, MainActivity::class.java))
-                        finish() // Tutup aktivitas saat ini
-                    }
-                    true
-                }
-                R.id.nav_test -> {
-                    if (javaClass != TestActivity::class.java) { // Periksa kelas saat ini
-                        startActivity(Intent(this, TestActivity::class.java))
-                        finish() // Tutup aktivitas saat ini
-                    }
-                    true
-                }
-                R.id.nav_result -> {
-                    // Pindah ke ResultActivity
-                    if (javaClass != ResultActivity::class.java) { // Periksa kelas saat ini
-                        startActivity(Intent(this, ResultActivity::class.java))
-                        finish()
-                    }
-                    true
-                }
-                R.id.nav_profile -> {
-                    // Pindah ke ProfileActivity
-                    if (javaClass != ProfileActivity::class.java) { // Periksa kelas saat ini
-                        startActivity(Intent(this, ProfileActivity::class.java))
-                        finish()
-                    }
-                    true
-                }
-                else -> false
-            }
-        }
-    }
 }
